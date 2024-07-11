2cbff26490bf5176959bd2078dc23f887d175566
naritya <140882526+naritya@users.noreply.github.com>
2024-07-11 21:23:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import FullNameDisplay from "./fullNameDisplay"; 
+
+function App() {
+  return (
+    <div>
+      <FullNameDisplay  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fullNameDisplay.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+const FullNameDisplay = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [text, setText] = useState("");
+
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // if (!firstName) alert("firstname is manadatory");
+          // else if (!lastName) alert("lastname is manadatory");
+          // else setText(`Full Name: ${firstName} ${lastName}`);
+        }}
+      >
+        <div>
+          First Name:
+          <input type="text" onChange={handleFirstName} required />
+        </div>
+        <div>
+          Last Name:
+          <input type="text" onChange={handleLastName} required />
+          <div>
+            <input type="submit" />
+          </div>
+        </div>
+      </form>
+      <p>{text}</p>
+    </>
+  );
+};
+
+export default FullNameDisplay ;
